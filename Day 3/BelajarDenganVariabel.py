3d9494e14d0fa7820adcc3055b24e8f17439af8b
Ghuulaam <ghuulaamm@gmail.com>
2024-06-03 18:56:05 +0700

    Create BelajarDenganVariabel.py
    - Day 3 - Belajar Dengan Variabel -

---
+++ Day 3/BelajarDenganVariabel.py
@@ -0,0 +1,27 @@
+# Mendifinisikan Variabel
+nama = "Ghulam"
+usia = 18
+tinggi = 173
+is_student = True
+
+# Menggunakan Variabel Dalam Operasi
+usia_tahun_depan = usia + 1
+tinggi_meter = tinggi / 100
+
+#Menampilkan Nilai Variabel
+print("Nama :", nama)
+print("Usia :", usia, "tahun")
+print("Usia Tahun Depan:", usia_tahun_depan, "tahun")
+print("Tinggi Dalam Meter:", tinggi_meter, "m")
+print("Apakah Seorang Pelajar? : ", is_student)
+
+#Mengubah Nilai Variabel
+nama = 'Cipto'
+usia = 40
+is_student = False
+
+#Menampilkan Nilai Variabel Setelah Di Ubah
+print("\nSetelah Beberapa Tahun...")
+print("Nama:", nama)
+print ("Usia:", usia, "tahun")
+print("Apakah Seorang Pelajar? : ", is_student)
