1a37a4400314e4a0369b3d407c6df1316a2d660f
Ghuulaam <ghuulaamm@gmail.com>
2024-06-06 19:05:16 +0700

    Create BelajarIfElifElse.py

---
+++ Day 5/BelajarIfElifElse.py
@@ -0,0 +1,10 @@
+x = 20
+
+if x > 30:
+    print("x lebih besar dari 10")
+elif x > 20:
+    print("x lebih besar dari 5 tetapi kurang dari atau sama dengan 10")
+elif x > 10:
+    print("x lebih besar dari 10 tetapi kurang dari atau sama dengan 20")
+else:
+    print("x kurang dari atau sama dengan 10")
