cecf1dde2e29eeb45e0a7ac3acc74a4f7a0b2048
Ghuulaam <ghuulaamm@gmail.com>
2024-06-10 17:22:53 +0700

    Create BelajarStringManiPulation.py
    Day 10 - Belajar String Manipulation

---
+++ Day 10/BelajarStringManiPulation.py
@@ -0,0 +1,4 @@
+pesan = "Halo, nama saya ghulam"
+print(pesan.upper()) # Untuk Mengubah Tulisan Menjadi Kapital
+print(pesan.lower()) # Untuk Mengubah Tulisan Menjadi Kecil
+print(pesan.split()) # Untuk Memisahkan Kalimat Tulisan
